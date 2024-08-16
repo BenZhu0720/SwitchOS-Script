1f407efc2c810bf3540057d17a71de4d92c078e1
BenZhu0720 <1421773529@qq.com>
2024-08-16 16:50:45 +0800

    new file:   reboot/__pycache__/convert_seconds.cpython-312.pyc 	new file:   reboot/convert_seconds.py 	modified:   reboot/reboot_2.0.py

---
+++ reboot/convert_seconds.py
@@ -0,0 +1,8 @@
+def convert_seconds(seconds):
+    days = seconds // (24 * 3600)
+    seconds %= (24 * 3600)
+    hours = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+    return days, hours, minutes, seconds
--- reboot/reboot_2.0.py
+++ reboot/reboot_2.0.py
@@ -5,8 +5,7 @@ import time
 import datetime
 
 
-max_retries = 40 #最大的尝试连接次数
-retry_interval = 3 #每次尝试之间的间隔时间(秒)
+from convert_seconds import convert_seconds
 
 device={
     'hostname': '192.168.1.123',
@@ -23,9 +22,10 @@ result_folder = "reboot_result"
 if not os.path.exists(result_folder):
     os.makedirs(result_folder)
 
-#初始化成功和失败次数的计数器
+#初始化成功和失败次数的计数器,详细失败次数的数组
 success_count = 0
 failure_count = 0
+fail_list = []
 
 #获取当前日期的时间戳
 current_date = datetime.datetime.now().strftime("%d%m%Y")
@@ -64,53 +64,60 @@ for i in range(reboot_count):
 
 
         #等待交换机重启(时间以秒为单位)
-        print(f"等待交换机重启......")
+        print(f"等待第{i+1}次交换机重启......")
         #等待一分钟后开始检测是否重启成功
         time.sleep(70)
 
         #尝试重新连接到交换机以检查其状态
-        retry_count = 18  # 设置重试次数
+        retry_count = 13  # 设置重试次数
         for retry in range(retry_count):
             try:
-                ssh.connect(device['hostname'], username=device['username'], password=device['password'])
+                ssh.connect(device['hostname'], username=device['username'], password=device['password'], timeout=7)
                 # 如果能够重新连接，则认为重启成功
                 success_count += 1
                 result = "交换机重启成功"
+                print(f"重试{retry+1}/{retry_count} - 成功重新连接到交换机")
                 break
             except Exception as e:
                 # 如果无法重新连接，继续尝试
                 print(f"重试{retry+1}/{retry_count} - 无法重新连接到交换机：{e}")
                 if retry < retry_count - 1:
-                    time.sleep(5)  # 等待一段时间后重试
+                    time.sleep(3)  # 等待一段时间后重试
                 else:
                     # 如果重试次数耗尽，则认为重启失败
                     failure_count += 1
                     result = "交换机重启失败"
+                    fail_list.append(i+1)
         
-        end_time = time.time()
-        reboot_time = end_time - start_time
-
     except paramiko.AuthenticationException as e:
         #处理认证失败的异常
-        print(f"初次认证失败： {e}")
+        print(f"用户名/密码错误认证失败： {e}")
         failure_count += 1
-        result = "初次登入认证失败"
+        result = "用户名/密码错误SSH登入认证失败"
+        fail_list.append(i+1)
         #暂停测试并等待用户继续输入测试
-        input("请解决初次认证失败问题,按Enter键继续测试......")
+        input(f"请解决第{i+1}次的用户名/密码错误SSH登入认证失败;\n按Enter键继续测试......")
     except paramiko.SSHException as e:
         #处理ssh异常
-        print(f"FIRST SSH CONNECT ERROR: {e}")
+        print(f"SSH CONNECT ERROR: {e}")
         failure_count += 1
-        result = "初次登入SSH ERROR"
+        result = "SSH CONNECT ERROR"
+        fail_list.append(i+1)
         #暂停测试并等待用户继续输入测试
-        input("请解决连接SSH失败问题,按Enter键继续测试......")
+        input(f"请解决第{i+1}次的SSH CONNECT ERROR问题;\n按Enter键继续测试......")
     except Exception as e:
         #处理其他异常
-        print(f"初次连接到交换机时出错： {e}")
+        print(f"OTHER ERROR: {e}")
         failure_count += 1
-        result = "初次登入其他异常"
+        result = "OTHER ERROR"
+        fail_list.append(i+1)
         #暂停测试并等待用户继续输入测试
-        input("请解决其他初次登入问题,按Enter键继续测试......")
+        input(f"请解决第{i+1}次的OTHER ERROR问题;\n按Enter键继续测试......")
+       
+       
+    end_time = time.time()
+    reboot_time = end_time - start_time
+    time.sleep(7)
 
     #将每次重启的结果保存到文件
     with open(f"{result_folder}/reboot_result_{current_date}.txt",'a') as file:
@@ -118,9 +125,13 @@ for i in range(reboot_count):
 
 end_test = time.time()
 all_test_time = end_time - start_test
+days, hours, minutes, seconds = convert_seconds(all_test_time)
 avg_test_time = all_test_time / reboot_count
 with open(f"{result_folder}/reboot_result_{current_date}.txt",'a') as file:
-        file.write(f"\n\n\n本次软重启测试,一共耗时:{all_test_time:.2f}秒\n大约平均每次重启耗时:{avg_test_time}秒\n")
+        file.write(f"\n\n\n#######\n本次软重启测试,一共耗时:{days:.0f}天,{hours:.0f}小时,{minutes:.0f}分钟,{seconds:.2f}秒\n平均每次重启耗时:{avg_test_time:.2f}秒\n#######\n")
+        if (failure_count > 0):
+           file.write(f"\n#######\n本次软重启测试一共出现{failure_count}次失败；\n重启失败在{fail_list}")
+
 #打印成功和失败次数
 print(f"重启成功次数: {success_count}")
 print(f"重启失败次数: {failure_count}")
